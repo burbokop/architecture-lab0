13d8821e6d654a534cdabc5ec93d63f19a0a0dfa
Borys Boiko <burbokop@gmail.com>
2020-10-02 15:43:41 +0300

    fgf

--- server.go
+++ server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Responce struct {
+	time string
+}
+
 func main() {
+
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World! time")
+		m := Responce{"Alice"}
+		b, err := json.Marshal(m)
+		fmt.Println(string(b))
+		fmt.Fprintf(w, "Hello World! time %s %s %s", b, string(b), err)
 	})
 	http.ListenAndServe(":4444", nil)
 }
