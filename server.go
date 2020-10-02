9dea96051aac623df9943262affcff91a193b5fd
Borys Boiko <burbokop@gmail.com>
2020-10-02 16:44:53 +0300

    some commit

--- server.go
+++ server.go
@@ -12,6 +12,8 @@ type Responce struct {
 }
 
 func main() {
+	fmt.Fprintf(w, "%s", "gogadoda")
+
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
 		b, err := json.Marshal(Responce{time.Now().Format(time.RFC3339)})
 		if err == nil {
